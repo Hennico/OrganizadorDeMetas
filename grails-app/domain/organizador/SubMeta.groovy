8367f93e1c087a8f126fe570747070f2557cb6e0
Hennico <nicolas_zanardo@hotmail.com>
2017-07-12 20:04:32 -0300

    Cambio en submenta, agregado de estados.

--- grails-app/domain/organizador/SubMeta.groovy
+++ grails-app/domain/organizador/SubMeta.groovy
@@ -1,7 +1,70 @@
 package organizador
 
 class SubMeta {
-
+	public Estado estado;
+	public List<SubMeta> subMetasOpocionales;
+	public List<SubMeta> subMetasObligatorias;
+	public List<Objetivo> listeners;
+	public String nombre;
+	public String descripcion;
+	
+	public SubMeta (String nombre, String descripcion) {
+		subMetasOpocionales = new List<SubMeta>();
+		subMetasObligatorias = new List<SubMeta>();
+		listeners = new List<Objetivo>();
+		
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+	
+	public void agregarSubMeta(SubMeta subMeta, boolean obligatorio) {
+		if (estado == Estado.Cancelado || estado == Estado.Terminado) {
+			throw new CustomException("No se puede agregar tarea portque esta finalizada/cancelada");
+		}
+		if (estado != Estado.Pendiente && obligatorio) {
+			throw new CustomException("No se puede agregar tarea obligatoria cuando ya se comenzo");
+		}
+		
+		if (obligatorio) {
+			subMetasObligatorias.add(subMeta);
+		} else {
+			subMetasOpocionales.add(subMeta);
+		}
+	}
+	
+	protected void informChange() {
+		for(Objetivo listener : listeners) {
+			listener.notify();
+		}
+	}
+	
+	protected boolean validarCambiarEstado(Estado nuevoEstado) {
+		switch(nuevoEstado) {
+			case Estado.Cancelado: 
+				return estado != Estado.Termiando;
+			
+			case Estado.Termiando:
+				if (estado != Estado.EnEjecucion) return false;
+				for(SubMeta opcional : subMetasOpocionales) {
+					if (opcional.estado != Estado.Finalizado || opcional.estado != Estado.Cancelado)
+						return false;
+				}
+				return true;
+				
+			case Estado.EnEjecucion:
+				if (estado != Estado.Pendiente) return false;
+				for(SubMeta opcional : subMetasObligatorias) {
+					if (opcional.estado != Estado.Finalizado || opcional.estado != Estado.Cancelado)
+						return false;
+				}
+				return true;
+			
+			case Estado.Pendiente: 
+				return false;
+		}
+		throw new Exception("El estado que se utilizo no es valido");
+	}
+	
     static constraints = {
     }
 }
