3c165bc77d79822a5d8f6d44e5af004e9b1c2d4b
FontiLucas <fontilucas@hotmail.com>
2017-07-13 18:26:38 -0300

    varias cosas. Compila y esta todo el comportamiento. Faltan los texto

--- grails-app/domain/organizador/Tarea.groovy
+++ grails-app/domain/organizador/Tarea.groovy
@@ -1,6 +1,27 @@
 package organizador
 
-class Tarea {
+class Tarea extends SubMeta{
+
+	
+	public void agregarSubMeta(SubMeta subMeta, boolean obligatorio) {
+		super.agregarSubMeta(subMeta, obligatorio)
+		subMeta.agragarListener(this)
+	}
+
+	public void Notify(){
+		bool flag
+		flag = true
+		for(SubMeta opcional : subMetasOpocionales) {
+			if (opcional.estado != Estado.Finalizado || opcional.estado != Estado.Cancelado)
+			flag = false;
+		}
+		for(SubMeta opcional : subMetasObligatorias) {
+			if (opcional.estado != Estado.Finalizado || opcional.estado != Estado.Cancelado)
+			flag = false;
+		}
+		if(flag)
+			estado = Estado.Termiando
+	}
 
     static constraints = {
     }
--- src/test/groovy/organizador/TareaSpec.groovy
+++ src/test/groovy/organizador/TareaSpec.groovy
@@ -15,4 +15,42 @@ class TareaSpec extends Specification implements DomainUnitTest<Tarea> {
         expect:"fix me"
             true == false
     }
+    
+    void testCambioEstadoPendienteAEnEjecucion() {
+		def tarea = new tarea()
+		tarea.estado = EnumEstado.Pendiente
+		tarea.CambiarEstado(EnumEstado.EnEjecucion)
+		
+        tarea.estado == EnumEstado.EnEjecucion
+    }
+ 
+    void testCambioEstadoPendienteAEnEjecucionConTareasPendientes() {
+		def tarea = new tarea()
+		def tarea2 = new tarea()
+ 		tarea.estado = EnumEstado.Pendiente
+ 		tarea.AgergarSubMeta(tara2, true)
+		tarea.CambiarEstado(EnumEstado.EnEjecucion)
+		
+		expect:"CambioEstadoInvalido"
+			true == false
+    }
+    
+    void testCambioEstadoEnEjecucionAEnEjecucion() {
+		def tarea = new tarea()
+		tarea.estado = EnumEstado.EnEjecucion
+		tarea.CambiarEstado(EnumEstado.EnEjecucion)
+		
+		expect:"CambioEstadoInvalido"
+			true == false
+    }
+    
+    void testCambioEstadoFinalizadaAEnEjecucion() {
+		def tarea = new tarea()
+		tarea.estado = EnumEstado.Finalizada
+		tarea.CambiarEstado(EnumEstado.EnEjecucion)
+		
+		expect:"CambioEstadoInvalido"
+			true == false
+    }
+    
 }
